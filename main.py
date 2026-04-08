fbb35f1def41c2202576862ddbf4da2d77c5da32
Charcot <xiake18010516721@163.com>
2026-04-08 10:26:07 +0800

    具备读取 .env 文件的能力

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from ui.main_window import MainWindow
-
+from dotenv import load_dotenv
+load_dotenv()
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
